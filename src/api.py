aba4af8ac71b6b3254382de86b56b5b8ba773e6e
liaoxiaoxuan <0angel.wisdom0@gmail.com>
2023-10-05 14:36:25 +0800

    name sheet

--- src/api.py
+++ src/api.py
@@ -39,6 +39,7 @@ print(sheet_data[0]['帳務時間'])
 
 myExcel = MyExcel()
 column_A = myExcel.sheet.range('A1').expand('down').value  # 把右邊裝到左邊（把右邊寫到左邊），把右邊一連串東東回傳的結果放到名為 column_A 的盒子
+sheet = myExcel.sheet  # 把右邊裝到左邊（把右邊寫到左邊），把右邊一連串東東回傳的結果放到名為 column_A 的盒子
 
 print(column_A)
 print(type(column_A))
@@ -52,11 +53,12 @@ else:
 startIndex_A = length_A+1  # 把右邊計算 length_A+1 的結果放到名為 startIndex_A 的盒子
 
 print(list(sheet_data[0].values())) # [0] 1. 代表取了 index（索引）為零的東西 2. （包含標題列在內的）第一列的內容
-googleHeader = ["帳務時間","類型","收入類別","消費內容","開銷內容","收入明細","支出明細","收入金額","入帳戶名","支出者","支出金額","付款方式","收入備註","支出備註"] 
+googleHeader = ["帳務時間","類型","收入類別","消費內容","開銷內容","收入明細","支出明細","收入金額","入帳戶名","付款方式","收入備註","支出備註"] 
 # 將欲填入 excel 裡面的 google 表單的 key 順序，命名為 googleHeader
 # googleHeader 就是 google sheet 的標題列
 # print([sheet_data[0][key] for key in googleHeader]) # list comprehension（列表生成式）
 
+row = 1
 inToExcel = []
 for key in googleHeader:
     # 按照 googleHeader 的順序去跑 key
@@ -64,11 +66,11 @@ for key in googleHeader:
     print(sheet_data[0][key])
     # 印出 key 對應的 value
 
-    if "支出者" == "曉仙":
-        sheet.range('H' + str(row)).value = "支出金額"
+    if sheet_data[0]["支出者"] == "曉仙":
+        sheet.range('H' + str(row)).value = sheet_data[0]["支出金額"]
         sheet.range('I' + str(row)).value = ""
-    elif "支出者" == "育玠":
-        sheet.range('I' + str(row)).value = "支出金額"
+    elif sheet_data[0]["支出者"] == "育玠":
+        sheet.range('I' + str(row)).value = sheet_data[0]["支出金額"]
         sheet.range('H' + str(row)).value = ""
     # 9/26：思考要如何把 google sheet 的 G 吃進來
     
@@ -76,6 +78,8 @@ for key in googleHeader:
         # 如果 key 對應的 value 是非空字串，再填入 value
         # 使用時機，如 google sheet 的「收入類別」、「消費內容」、「開銷內容」三欄的內容，都要視時機填入 Excel 的「類別」
         inToExcel.append(sheet_data[0][key])
+    else:
+        # 如果 key 對應的 value 是空字串，需要塞某個東西填入
 print(inToExcel)
 
 # 將 value 填入 excel 對應的行
